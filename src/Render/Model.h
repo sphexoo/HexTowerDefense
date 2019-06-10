7ec8125802874c30472ae3f6c538a1fc58419156
sphexoo <fabian_sauer@freenet.de>
2019-06-10 19:24:37 +0200

    Commended model class.

--- src/Render/Model.h
+++ src/Render/Model.h
@@ -22,7 +22,7 @@ public:
 
 	void LoadObj(std::string filepath);
 	void LoadPly(std::string filepath);
-	void LoadVertices(std::vector<float>& vertices, std::vector<unsigned int> indices);
+	void LoadVertices(std::vector<float>& vertices, std::vector<unsigned int> indices, unsigned int iuSlot0, unsigned int iuSlot1, unsigned int iuSlot2);
 	void Draw(Renderer& renderer, glm::mat4& viewMatrix, glm::mat4& modelMatrix);
 	void DrawDepth(Renderer& renderer, glm::mat4& viewMatrix, glm::mat4& modelMatrix);
 	void ModifyVB(std::vector<float>& vertices);
